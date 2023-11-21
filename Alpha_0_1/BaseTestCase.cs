46a9776d7afa62ca299c6e5a39c686f97d42529f
TestResults.io <paveportal@progile.ch>
2023-11-21 11:03:56 +0100

    Add base test case

---
+++ Alpha_0_1/BaseTestCase.cs
@@ -0,0 +1,39 @@
+﻿using Progile.ATE.TestFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alpha_Model
+{
+    public class TestCase
+    {
+        protected AlphaApp App { get; set; }
+
+        [SetupTest]
+        public virtual bool Setup(ITester t)
+        {
+            App = new AlphaApp(t);
+            return true;
+        }
+
+        //[PreconditionStep(
+        //    TestInput = "",
+        //    ExpectedResults = "")]
+        //public virtual void PreconditionStep(ITester t)
+        //{ }
+
+        //[CleanupStep(
+        //    TestInput = "",
+        //    ExpectedResults = "")]
+        //public virtual void CleanupStep(ITester t)
+        //{ }
+
+        //[TearDownTest]
+        //public virtual bool TearDown(ITester t)
+        //{
+        //    return true;
+        //}
+    }
+}
